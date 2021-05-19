82738c90639bb20c38e7fbe9aacad53a8f1622d7
Jorge Salhani <jorge.salhani@usp.br>
2021-05-19 15:38:06 -0300

    Add files via upload

---
+++ Module4/source.js
@@ -0,0 +1,13 @@
+// console.log(
+//   document.body.childNodes[4]
+// )
+
+let buttonHome = document.createElement('button');
+buttonHome.innerHTML = 'HOME';
+
+let body = document.getElementsByTagName('body')[0];
+body.appendChild(buttonHome);
+
+buttonHome.addEventListener('click', function () {
+  
+})
